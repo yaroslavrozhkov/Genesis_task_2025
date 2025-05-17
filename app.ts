5fed42b2c99f7b7d447e5409602ccb4feaf24c39
OFFICEKIEV\y.rozhkov <gitlab.ci@temabit.com>
2025-05-17 22:01:04 +0300

    feat: added web page

--- app.ts
+++ app.ts
@@ -1,9 +1,14 @@
 import express from "express";
-import apiRoutes from "./routes/api";
+import apiRoutes from "./src/routes/api";
+import path from "path";
 
 const app = express();
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api", apiRoutes);
+app.use("/", express.static(path.join(__dirname, "public")));
 
 const PORT = 3000;
 app.listen(PORT, () => {
